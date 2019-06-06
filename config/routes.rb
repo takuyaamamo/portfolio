bba61ddcfad4b2058eb13bb36e6cf63a36f91b93
takuyaamamo <iamaama703@gmail.com>
2019-06-06 16:55:13 +0900

    [update]rootをitemのindexに設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  # トップページをitemsのindexに設定
+  root 'items#index'
+
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
