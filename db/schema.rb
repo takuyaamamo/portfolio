3c9fc1f3ea01a029f33b67b5f1df07343d3ddea0
takuyaamamo <iamaama703@gmail.com>
2019-06-06 18:02:15 +0900

    [create]画像投稿用のmigrateとitemへのアソシエーション設定完了

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_081637) do
+ActiveRecord::Schema.define(version: 2019_06_06_085524) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2019_06_06_081637) do
     t.string "item_qr"
     t.integer "item_price"
     t.integer "item_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "image_id"
+    t.integer "item_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
