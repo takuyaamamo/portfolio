edb68fde00682333e101e5453bfa5c19654efc87
takuyaamamo <iamaama703@gmail.com>
2019-06-08 01:46:05 +0900

    [create]routeでadmin配下にitemsを設置。それに伴いview,helper,test,controllerを新規作成&設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
 
   devise_for :admins
+
   # トップページをitemsのindexに設定
   root 'items#index'
 
+  # namespaceでadmin配下にitemsを配置
+  namespace :admin do
+    resources :items
+  end
+  # 一般のページ
   resources :items
 
+  # refilegem用
   resources :post_images, only: [:new, :create, :index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
