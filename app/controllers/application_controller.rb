3fb70708d2312b2b1d63d719a27ff21765ed4097
takuyaamamo <iamaama703@gmail.com>
2019-06-08 01:46:54 +0900

    [create]application_controllerをadmin配下に継承する設定を記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:admin_name])
     # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
+
+  # sign_in後のリダイレクト先設定
+  def after_sign_in_path_for(resource)
+    admin_items_path
+  end
+  # sign_out後のリダイレクト先設定
+  def after_sign_out_path_for(resource)
+    root_path
+  end
   # ============================================================================
 
 end
