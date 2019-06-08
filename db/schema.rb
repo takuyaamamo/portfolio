b28a57f61a5e98fc88330872fe305311d4ea6565
takuyaamamo <iamaama703@gmail.com>
2019-06-08 16:39:20 +0900

    [create]Stockモデルを作成itemモデルと1:1のアソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_06_085524) do
+ActiveRecord::Schema.define(version: 2019_06_08_073045) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -39,6 +39,13 @@ ActiveRecord::Schema.define(version: 2019_06_06_085524) do
   create_table "post_images", force: :cascade do |t|
     t.text "image_id"
     t.integer "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "stocks", force: :cascade do |t|
+    t.integer "item_id"
+    t.integer "stock_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
