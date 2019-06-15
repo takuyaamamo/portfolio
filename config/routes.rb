b73683692a41f603e4d6459f8c102da8dbba2d10
takuyaamamo <iamaama703@gmail.com>
2019-06-15 15:03:58 +0900

    [update]不要なルーティング、ビュー、コントローラを削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :admins
+
+  # =========================デバイスの機能を絞る設定==============================
+  devise_for :admins, skip: :all
+
+  devise_scope :admin do
+    get 'login', to: 'devise/sessions#new', as: 'new_admin_session'
+    post 'login', to: 'devise/sessions#create', as: 'admin_session'
+    delete 'logout', to: 'devise/sessions#destroy', as: 'destroy_admin_session'
+  end
+  # ===========================================================================
 
   # トップページをitemsのindexに設定
   root 'items#index'
@@ -7,14 +16,15 @@ Rails.application.routes.draw do
   # namespaceでadmin配下にitemsを配置
   namespace :admin do
     resources :items
-    resources :purchased_histories
+    resources :purchased_histories, only: [:index, :show]
     put 'statuschange/:id', to: 'items#statuschange', as: 'statuschange'
     get 'confirm/:id', to: 'items#confirm', as: 'confirm'
     put 'shippingchange/:id', to: 'purchased_histories#shippingchange', as: 'shippingchange'
   end
+
   # 一般のページ
-  resources :items
-  resources :purchased_histories
+  resources :items, only: [:index, :show]
+  resources :purchased_histories, only: [:new, :create]
   post 'purchased_histories/sessioncreate', to: 'purchased_histories#sessioncreate', as: 'sessioncreate'
   get 'purchased_histories/sessiondestroy/:id', to: 'purchased_histories#sessiondestroy', as: 'sessiondestroy'
 
